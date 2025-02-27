14013853416bc6ce3f01e8106680e65b239e32d6
ke0ta-kawaguchi <kawaguchi@sikmi.com>
2025-02-27 18:44:31 +0900

    add slim rspec

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RailsPg
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators.template_engine = :slim
   end
 end
